a4035aa9bfe2abc6ce15f4ed1088d204b6159ec3
egrfly <egrfly@outlook.com>
2022-07-19 19:09:11 +0100

    Add copy button to code text area pages

--- src/components/CodeFullPageTextArea.tsx
+++ src/components/CodeFullPageTextArea.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 import CodeMirror from 'codemirror'
+import { Tooltip } from 'bootstrap'
 import { LanguageContext } from './Presentation'
 
 interface CodeFullPageTextAreaProps {
@@ -8,10 +9,13 @@ interface CodeFullPageTextAreaProps {
 }
 
 const CodeFullPageTextArea = ({code, language}: CodeFullPageTextAreaProps) => {
+  const copyButton = useRef<HTMLButtonElement>(null)
   const snippet = useRef<HTMLTextAreaElement>(null)
 
   const languageFromContext = useContext(LanguageContext)
   const editorLanguage = language || languageFromContext
+
+  const [copyButtonTooltip, setCopyButtonTooltip] = useState<Tooltip>()
 
   useEffect(() => {
     CodeMirror.fromTextArea(snippet.current!, {
@@ -21,8 +25,25 @@ const CodeFullPageTextArea = ({code, language}: CodeFullPageTextAreaProps) => {
     })
   }, [editorLanguage])
 
+  useEffect(() => {
+    setCopyButtonTooltip(new Tooltip(copyButton.current as Element, {
+      title: "Copy code",
+      placement: "left",
+      trigger: "hover",
+      customClass: "tooltip-primary"
+    }))
+  }, [])
+
+  const copyCode = () => {
+    navigator.clipboard.writeText(code);
+    copyButton.current?.setAttribute('data-bs-original-title', "Copied!");
+    copyButtonTooltip?.show()
+    copyButton.current?.setAttribute('data-bs-original-title', "Copy code");
+  }
+
   return (
-    <div className="w-100 py-3 code-mirror-block">
+    <div className="w-100 mt-3 pb-3 code-mirror-block">
+      <button className="btn btn-primary code-mirror-action" ref={copyButton} onClick={copyCode}><i className="bi bi-clipboard2-fill"></i></button>
       <textarea className="bg-dark" ref={snippet} defaultValue={code} />
     </div>
   )
